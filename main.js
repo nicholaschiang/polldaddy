c5c1f584cd823b9509983a6d42d63fe066e040fe
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-09-21 15:58:10 -0700

    randomize user agent and add 6 sec wait

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
+const { JSDOM } = require('jsdom');
 const { to } = require('await-to-js');
 const progress = require('cli-progress');
+const randomUserAgent = require('random-useragent');
 const Bottleneck = require('bottleneck');
 
-async function main(total = 100) {
+async function main(total = 10) {
   console.log(`Voting ${total} times...`);
-  const limiter = new Bottleneck({ maxConcurrent: 100 });
+  // Wait 6secs in between votes because of Poll Daddy's rate limits.
+  // @see {@link https://bit.ly/2W0xzsN}
+  const limiter = new Bottleneck({ maxConcurrent: 1, minTime: 6000 });
   let count = 0;
   const bar = new progress.SingleBar({}, progress.Presets.shades_classic);
   bar.start(total, count);
@@ -20,23 +24,33 @@ async function main(total = 100) {
   limiter.on('retry', (error, jobInfo) => {
     console.log(`Now retrying job (${jobInfo.options.id})...`);
   });
-  await Promise.all(Array(total).fill(null).map((_, id) => limiter.schedule({ id }, axios, {
-      method: 'get',
-      url: 'https://poll.fm/vote',
-      params: {
-        va: 20,
-        pt: 0,
-        r: 0,
-        p: 10924113,
-        a: '50270630,',
-        o: '',
-        //t: 10427, // Changes
-        //token: 'b1ef6dc64e1be8eb1b8496f5ed989126', // Changes
-        //pz: 152, // Changes
-      },
-    })));
+  await Promise.all(
+    Array(total).fill(null).map((_, id) =>
+      limiter.schedule({ id }, async () => {
+        const cfg = { headers: { 'User-Agent': randomUserAgent.getRandom() } };
+        const { data: html } = await axios.get('https://poll.fm/10924113', cfg);
+        const doc = new JSDOM(html).window.document;
+        const btn = doc.querySelector('.vote-button');
+        const data = JSON.parse(btn.dataset.vote);
+        await axios.get('https://poll.fm/vote', {
+          params: {
+            va: 20,
+            pt: 0,
+            r: 0,
+            p: 10924113,
+            a: '50270630,',
+            o: '',
+            t: data.t,
+            token: data.n,
+            pz: 1,
+          },
+          ...cfg
+        });
+      })
+    )
+  );
   bar.stop();
   console.log(`Voted ${count}/${total} times.`);
 }
 
-main(500);
+main(50);
